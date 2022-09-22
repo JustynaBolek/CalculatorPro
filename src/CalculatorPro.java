8d4e599b601960b1735e4748edb9983200f09f0a
JustynaBolek <justyna.bolek@gmail.com>
2022-09-23 00:50:49 +0200

    added a new methods addition, substraction, multiplication, division

---
+++ src/CalculatorPro.java
@@ -0,0 +1,15 @@
+public class CalculatorPro {
+    public int addition(int x, int y){
+        return x+y;
+    }
+    public int substraction(int x, int y){
+        return x=y;
+    }
+
+    public int multiplication(int x, int y){
+        return x*y;
+    }
+    public int division(int x, int y){
+        return x/y;
+    }
+}
